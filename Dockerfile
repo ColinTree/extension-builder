def438d1504fef568ee60c45a7877d02eff199e3
ColinTree <502470184@qq.com>
2019-02-28 17:08:14 +0800

    Docker test

---
+++ Dockerfile
@@ -0,0 +1,24 @@
+FROM frekele/ant
+
+RUN apt-get update -yq \
+    && apt-get install curl gnupg -yq \
+    && curl -sL https://deb.nodesource.com/setup_8.x | bash \
+    && apt-get install nodejs -yq \
+    && apt-get install -y git
+
+RUN npm i -g pm2 \
+    && git clone https://github.com/mit-cml/appinventor-sources.git /usr/workspace \
+    && cd /use/workspace \
+    && git submodule init \
+    && git submodule update
+
+WORKDIR /usr/src
+
+COPY package*.json .
+RUN npm i
+
+COPY . .
+
+EXPOSE 8048
+
+CMD [ "npm", "start" ]
--- index.js
+++ index.js
@@ -2,34 +2,17 @@ let http = require("http");
 let exec = require("./exec");
 
 const PORT = 8048;
-const AI_WORKSPACE = "/var/extension-builder/workspace/appinventor-sources";
-const AI_GITHUB_URL = "https://github.com/mit-cml/appinventor-sources.git";
-const GIT_FOLDER_NAME = ".git";
+const AI_WORKSPACE = "/usr/workspace";
 
-ensureAiSourceExist();
-
-function ensureAiSourceExist() {
-  exec("cd " + AI_WORKSPACE + "/" + GIT_FOLDER_NAME)
-  .then(() => {
-    console.log("ai repo exists in " + AI_WORKSPACE);
-    exec("cd " + AI_WORKSPACE + " && " + "git pull")
-    .then(() => startServer());
-  })
-  .catch(() => {
-    exec("cd " + AI_WORKSPACE + " && git clone " + AI_GITHUB_URL)
-    .then(() => {
-      ensureAiSourceExist();
-    });
-  });
-}
+startServer();
 
 function startServer() {
   let deployServer = http.createServer((request, response) => {
-    response.writeHead(200);
-    response.end("Not available for use yet.");
-
-    //exec("git reset --hard HEAD && git clean -f")
-    //.then(stdout => {});
+    exec("cd " + AI_WORKSPACE + " && git status && git reset --hard HEAD && git clean -f && git status")
+    .then(stdout => {
+      response.writeHead(200);
+      response.end(stdout);
+    });
   });
   console.log("listening port at: " + PORT);
   deployServer.listen(PORT);
