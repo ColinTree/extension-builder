333122e4cf12cf670c458667ef67c87eb556a2ac
ColinTree <502470184@qq.com>
2019-03-05 19:33:45 +0800

    Handled check-status &result when job not exist in pool but result exist

--- src/pages/check-status.ts
+++ src/pages/check-status.ts
@@ -2,14 +2,15 @@ import { IncomingMessage, ServerResponse } from "http";
 import { URLSearchParams } from "url";
 
 import { responseSuccess, responseError } from "../index";
-import { JobPool } from "../builder";
+import { JobPool, JobStatus } from "../builder";
+import * as fs from "fs-extra";
+import { OUTPUT_DIR } from "../config";
 
 export default (request: IncomingMessage, response: ServerResponse, params: URLSearchParams) => {
   let jobId = params.get("jobId");
-  if (!JobPool.has(jobId)) {
-    // TODO: try harder to find this job in "build-result"?
+  if (!JobPool.has(jobId) && !fs.existsSync(OUTPUT_DIR + "/" + jobId + ".zip")) {
     responseError(response, 404, "Specified job does not exist in job pool.");
     return;
   }
-  responseSuccess(response, { status: JobPool.get(jobId).status });
+  responseSuccess(response, { status: JobPool.has(jobId) ? JobPool.get(jobId).status : JobStatus.done });
 }
--- src/pages/result.ts
+++ src/pages/result.ts
@@ -3,19 +3,19 @@ import * as fs from "fs-extra";
 import { IncomingMessage, ServerResponse } from "http";
 import { URLSearchParams } from "url";
 
-import { JobPool } from "../builder";
+import { JobPool, JobStatus } from "../builder";
 import { OUTPUT_DIR } from "../config";
 
 export default (request: IncomingMessage, response: ServerResponse, params: URLSearchParams) => {
   let jobId = params.get("jobId");
-  if (!JobPool.has(jobId)) {
+  if (!JobPool.has(jobId) && !fs.existsSync(OUTPUT_DIR + "/" + jobId + ".zip")) {
     console.log("Response end with 404: job not exist");
     response.writeHead(404);
     response.end("Job does not exist.");
     return;
   }
-  let status = JobPool.get(jobId).status;
-  if (status != "done") {
+  let status = JobPool.has(jobId) ? JobPool.get(jobId).status : JobStatus.done;
+  if (status != JobStatus.done) {
     console.log("Response end with 404 job is not ready yet");
     response.writeHead(404);
     response.end("Job not ready yet.");
