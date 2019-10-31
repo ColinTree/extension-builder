3f4b0fdc1f0ceebacb1b42990eb8695347e4eaf2
Colin Yang <502470184@qq.com>
2019-10-31 15:14:41 +1100

    Use koa-send to send result

--- src/pages/result.ts
+++ src/pages/result.ts
@@ -1,9 +1,10 @@
 import * as fs from 'fs-extra';
 import { Context } from 'koa';
+import send = require('koa-send');
 import { CHECK_JOBPOOL_RESULTS_ONLY, OUTPUT_DIR } from '../configs';
 import JobPool from '../JobPool.class';
 
-export default (ctx: Context) => {
+export default async (ctx: Context) => {
   const jobId = ctx.query.jobId as string;
   if (!JobPool.has(jobId) && !CHECK_JOBPOOL_RESULTS_ONLY && !fs.existsSync(`${OUTPUT_DIR}/${jobId}.zip`)) {
     ctx.throw(404, 'Job not exist.');
@@ -13,10 +14,8 @@ export default (ctx: Context) => {
     ctx.throw(404, 'Job not ready yet.');
   } else {
     const zipPath = `${OUTPUT_DIR}/${jobId}.zip`;
-    const contentLength = fs.statSync(zipPath).size;
     ctx.status = 200;
-    ctx.type = 'application/zip';
-    ctx.set('Content-Length', String(contentLength));
-    ctx.body = fs.createReadStream(zipPath);
+    ctx.attachment(`eb-result-${jobId}.zip`);
+    await send(ctx, zipPath, { root: '/' });
   }
 };
